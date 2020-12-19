8c1e5c351ec37367a7abb343d7fc60631f2e79be
ashirsh <asmdot@gmail.com>
2020-12-19 14:43:10 +0300

    add push add clear add pop

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -4,6 +4,9 @@ My little Stack
 from typing import Any
 
 
+my_stack = []  # вершина справа
+
+
 def push(elem: Any) -> None:
     """
     Operation that add element to stack
@@ -11,7 +14,7 @@ def push(elem: Any) -> None:
     :param elem: element to be pushed
     :return: Nothing
     """
-    print(elem)
+    my_stack.append(elem)
     return None
 
 
@@ -21,6 +24,7 @@ def pop() -> Any:
 
     :return: popped element
     """
+    my_stack.pop()
     return None
 
 
@@ -41,4 +45,15 @@ def clear() -> None:
 
     :return: None
     """
+    my_stack.clear()
     return None
+
+
+if __name__ == '__main__':
+    print(my_stack)
+    push(0)
+    print(my_stack)
+    push(1)
+    print(my_stack)
+    pop()
+    print(my_stack)
