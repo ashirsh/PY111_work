509ab3ce635ea0324f8e01046d4742e56cab8f5f
ashirsh <asmdot@gmail.com>
2020-12-19 19:58:34 +0300

    add check_brackets

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,16 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
+    count, i = 0, 0
+    while count >= 0 and i < len(brackets_row):
+        if brackets_row[i] == '(':
+            count += 1
+        if brackets_row[i] == ')':
+            count -= 1
+        i += 1
+    return not bool(count)
 
-    return False
+
+if __name__ == '__main__':
+    for row in ["", "()", "()()(()())", "(", ")", ")("]:
+        print(check_brackets(row))
