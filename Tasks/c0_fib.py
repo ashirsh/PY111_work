4cd8077a2d1468e8ae5aa6bdc7da2f499a1d490f
ashirsh <asmdot@gmail.com>
2020-12-26 15:02:51 +0300

    add fib_recursive

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,8 +5,14 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n < 0:
+        raise ValueError
+    else:
+        return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 
 def fib_iterative(n: int) -> int:
@@ -18,3 +24,7 @@ def fib_iterative(n: int) -> int:
     """
     print(n)
     return 0
+
+
+if __name__ == '__main__':
+    print(fib_recursive(7))
