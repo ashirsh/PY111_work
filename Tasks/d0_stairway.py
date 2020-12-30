8a6eb84210807815cf8b39d99a0ad3dec9e2499a
ashirsh <asmdot@gmail.com>
2020-12-30 18:43:04 +0300

    add stairway_path

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,5 +8,16 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
-    print(stairway)
-    return 0
+    price = [float('inf')] * len(stairway)
+    price[0] = stairway[0]
+    price[1] = stairway[1]
+
+    for i in range(2, len(stairway)):
+        price[i] = min(price[i-2], price[i-1]) + stairway[i]
+
+    return price[-1]
+
+
+if __name__ == '__main__':
+    test = [1, 0, 1, 1, 0, 2, 0, 1, 1, 2]
+    print(stairway_path(test))
